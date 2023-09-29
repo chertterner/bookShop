23b2e16e72bcc2700f52793935230be492aa8be3
Sergei <chertterner686@gmail.com>
2023-09-29 15:55:29 +0300

    added book entity , book repo

---
+++ src/main/java/com/example/bookshop/repository/implementation/BookRepositoryImpl.java
@@ -0,0 +1,54 @@
+package com.example.bookshop.repository.implementation;
+
+import com.example.bookshop.exceptions.DataProcessingException;
+import com.example.bookshop.model.Book;
+import com.example.bookshop.repository.BookRepository;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Transactional
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert a cinema hall: " + book, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaQuery<Book> criteriaQuery = session.getCriteriaBuilder()
+                    .createQuery(Book.class);
+            criteriaQuery.from(Book.class);
+            return session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get all cinema halls", e);
+        }
+    }
+}
---
+++ src/main/java/com/example/bookshop/service/implementation/BookServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.bookshop.service.implementation;
+
+import com.example.bookshop.model.Book;
+import com.example.bookshop.repository.BookRepository;
+import com.example.bookshop.service.BookService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
