ae53a778aac7b0ae879f51bee6f0e0d9721a3c42
Sergei <chertterner686@gmail.com>
2023-10-06 16:07:46 +0300

    added web controller, mapstruct

--- src/main/java/com/example/bookshop/repository/implementation/BookRepositoryImpl.java
+++ src/main/java/com/example/bookshop/repository/implementation/BookRepositoryImpl.java
@@ -1,52 +1,52 @@
 package com.example.bookshop.repository.implementation;
 
 import com.example.bookshop.exceptions.DataProcessingException;
+import com.example.bookshop.exceptions.EntityNotFoundException;
 import com.example.bookshop.model.Book;
 import com.example.bookshop.repository.BookRepository;
-import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import java.util.List;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookRepositoryImpl implements BookRepository {
     @Autowired
-    private SessionFactory sessionFactory;
+    private EntityManager entityManager;
 
     @Override
     public Book save(Book book) {
-        Session session = null;
-        Transaction transaction = null;
+        EntityTransaction transaction = null;
         try {
-            session = sessionFactory.openSession();
-            transaction = session.beginTransaction();
-            session.persist(book);
+            transaction = entityManager.getTransaction();
+            entityManager.persist(book);
             transaction.commit();
             return book;
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't insert a cinema hall: " + book, e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
+            throw new DataProcessingException("Can't insert a book: " + book, e);
         }
     }
 
     @Override
     public List<Book> findAll() {
-        try (Session session = sessionFactory.openSession()) {
-            CriteriaQuery<Book> criteriaQuery = session.getCriteriaBuilder()
-                    .createQuery(Book.class);
-            criteriaQuery.from(Book.class);
-            return session.createQuery(criteriaQuery).getResultList();
+        try {
+            return entityManager.createNativeQuery("SELECT * FROM book", Book.class)
+                    .getResultList();
         } catch (Exception e) {
-            throw new DataProcessingException("Can't get all cinema halls", e);
+            throw new EntityNotFoundException("Can't get all books ", e);
+        }
+    }
+
+    @Override
+    public Book getById(Long id) {
+        try {
+            return entityManager.find(Book.class, id);
+        } catch (Exception e) {
+            throw new EntityNotFoundException("Can't get all book by id : " + id, e);
         }
     }
 }
