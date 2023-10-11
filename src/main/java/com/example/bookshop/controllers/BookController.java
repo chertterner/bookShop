aefa02d2e99f047550a9b8958d3d1a9f24ae928e
Sergei <chertterner686@gmail.com>
2023-10-11 20:37:21 +0300

    Book validation

--- src/main/java/com/example/bookshop/controllers/BookController.java
+++ src/main/java/com/example/bookshop/controllers/BookController.java
@@ -3,9 +3,8 @@ package com.example.bookshop.controllers;
 import com.example.bookshop.dto.BookDto;
 import com.example.bookshop.dto.CreateBookRequestDto;
 import com.example.bookshop.service.BookService;
-import java.util.List;
-
 import jakarta.validation.Valid;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
