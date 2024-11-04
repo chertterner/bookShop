9e0f586484f9dfc9aaa22ba2ffc53014479816a6
Sergei <chertterner686@gmail.com>
2024-11-04 19:36:50 +0200

    first commit with Category

--- src/main/java/com/example/bookshop/controller/CategoryController.java
+++ src/main/java/com/example/bookshop/controller/CategoryController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Category management", description = "Endpoints for manage categories")
+@Tag(name = "Category management", description = "Endpoints for categories")
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
